45e8149a4e992cc4528a1207c9ec2157992d80db
AdiRomanov <romanovadi44@gmail.com>
2024-05-21 01:30:41 +0300

    deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'https://fastapi-adi-licenta-81a7cfa1c5b5.herokuapp.com' // Production API URL
+  };
+  
