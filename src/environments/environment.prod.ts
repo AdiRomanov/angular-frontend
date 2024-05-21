537ec84fc064171182e1f9e8377dadb4b825145f
AdiRomanov <romanovadi44@gmail.com>
2024-05-21 17:28:53 +0300

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-    production: true,
-    apiUrl: 'https://fastapi-adi-licenta-81a7cfa1c5b5.herokuapp.com' // Production API URL
-  };
-  
